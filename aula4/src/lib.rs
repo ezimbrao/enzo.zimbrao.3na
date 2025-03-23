4a5240e607ccedf880b1f91932b411134b1e7194
Enzo Zimbrão <zimbraoenzo7@gmail.com>
2025-03-23 15:53:46 +0000

    Desafio Rust

---
+++ aula4/src/lib.rs
@@ -0,0 +1,27 @@
+// src/lib.rs
+
+pub unsafe fn multiply_array(ptr: *const i32, len: usize) -> i32 {
+    let mut product = 1;
+    for i in 0..len {
+        product *= *ptr.offset(i as isize);
+    }
+    product
+}
+// o erro estava no indice que começava do 1 nao do 0 pulando o primeiro elemento do vetor 
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_multiply_array() {
+        let array = [2, 3, 4]; // Produto: 2*3*4 = 24
+        unsafe {
+            assert_eq!(multiply_array(array.as_ptr(), array.len()), 24);
+        }
+    }
+}
+
